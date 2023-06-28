ea5256d340daf83111684453d1f113d4853035a9
Angathan003 <angathan003@gmail.com>
2023-06-28 20:01:22 +0530

    add date and weekday

--- Degital clock/app.js
+++ Degital clock/app.js
@@ -1,9 +1,20 @@
-let ampm =document.getElementById('ampm')
+let ampm =document.getElementById('ampm');
+let dateElement = document.getElementById('date');
+let dayElement = document.getElementById('day');
+
+
 function display(){
     let dateTime = new Date();
     let hour = dateTime.getHours()
     let min = addzero(dateTime.getMinutes())
     let sec = addzero(dateTime.getSeconds())
+    let date = dateTime.toLocaleDateString(undefined, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      });
+    let day = dateTime.toLocaleDateString(undefined, { weekday: 'long' });
+
     
     if(hour>12){
         hour = hour - 12
@@ -14,6 +25,8 @@ function display(){
     document.getElementById("hours").innerHTML = addzero(hour)
     document.getElementById("mins").innerHTML = min   
     document.getElementById("seconds").innerHTML = sec
+    dateElement.innerHTML = date;
+    dayElement.innerHTML = day;
 }
 function addzero(n){
     return n<10?"0"+n:n
