88c5864843a87dee7bfcf80d1d08eff7f606cbe0
Angathan003 <angathan003@gmail.com>
2023-07-05 18:17:21 +0530

    add Clock

--- Degital clock/app.js
+++ Degital clock/app.js
@@ -1,78 +1,76 @@
-let ampm =document.getElementById('ampm');
+let ampm = document.getElementById('ampm');
 let dateElement = document.getElementById('date');
 let dayElement = document.getElementById('day');
 let countryElement = document.getElementById('country');
+let clockElement = document.getElementById('clock');
 
-function display(){
-    let dateTime = new Date();
-    let hour = dateTime.getHours()
-    let min = addzero(dateTime.getMinutes())
-    let sec = addzero(dateTime.getSeconds())
-    // let date = dateTime.toLocaleDateString(undefined, {
-    //     year: 'numeric',
-    //     month: 'long',
-    //     day: 'numeric'
-    //   });
-    // let day = dateTime.toLocaleDateString(undefined, { weekday: 'long' });
-
+function display() {
+  let dateTime = new Date();
+  let hour = dateTime.getHours();
+  let min = dateTime.getMinutes();
+  let sec = dateTime.getSeconds();
+  let options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+  };
+  fetch('https://ipapi.co/json/')
+  .then(response => response.json())
+  .then(data => {
+    let country = data.country_name;
+    let timeZone = data.timezone;
+    let date = dateTime.toLocaleDateString(undefined, options);
     
-    // if(hour>12){
-    //     hour = hour - 12
-    //     ampm.innerHTML = 'PM'
+    // Adjusting the time based on the country's time zone
+    let countryDateTime = dateTime.toLocaleString('en-US', {
+      timeZone: timeZone,
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true
+      });
 
-    // }
-
-    let options = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        weekday: 'long',
-        // timeZoneName: 'short'
-      };
       
-      fetch('https://ipapi.co/json/')
-        .then(response => response.json())
-        .then(data => {
-          let country = data.country_name;
-          let timeZone = data.timezone;
-          let date = dateTime.toLocaleDateString(undefined, options);
-          
-          // Adjusting the time based on the country's time zone
-          let countryDateTime = dateTime.toLocaleString('en-US', {
-            timeZone: timeZone,
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric',
-            hour12: true
-          });
-          
-          let [countryHour, countryMin, countrySec] = countryDateTime.split(':');
-          hour = parseInt(countryHour);
-          min = parseInt(countryMin);
-          sec = parseInt(countrySec);
-          
-          countryElement.innerHTML = country;
-          dateElement.innerHTML = date;
-          dayElement.innerHTML = dateTime.toLocaleDateString(undefined, { weekday: 'long' });
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
+      if (hour >= 12) {
+        ampm.innerHTML = 'PM';
+      } else {
+        ampm.innerHTML = 'AM';
+      }    
 
-         if(hour>12){
-        hour = hour - 12
-        ampm.innerHTML = 'PM'
+      let [countryHour, countryMin, countrySec] = countryDateTime.split(':');
+      hour = parseInt(countryHour);
+      min = parseInt(countryMin);
+      sec = parseInt(countrySec);
+      countryElement.innerHTML = country;
+      dateElement.innerHTML = date;
+      dayElement.innerHTML = dateTime.toLocaleDateString(undefined, { weekday: 'long' });
 
-        }
 
-    document.getElementById("hours").innerHTML = addzero(hour)
-    document.getElementById("mins").innerHTML = min   
-    document.getElementById("seconds").innerHTML = sec
-    // dateElement.innerHTML = date;
-    // dayElement.innerHTML = day;
-}
-function addzero(n){
-    return n<10?"0"+n:n
+
+      document.getElementById("hours").innerHTML = addZero(hour);
+      document.getElementById("mins").innerHTML = addZero(min);
+      document.getElementById("seconds").innerHTML = addZero(sec);
+
+      updateClock(hour, min, sec);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
 
-setInterval(display,100)
+function addZero(n) {
+  return n < 10 ? "0" + n : n;
+}
+
+function updateClock(hour, min, sec) {
+  let rotationHour = (hour % 12) * 30 + min * 0.5;
+  let rotationMin = min * 6 + sec * 0.1;
+  let rotationSec = sec * 6;
+
+  document.getElementById("hour-hand").style.transform = `rotate(${rotationHour}deg)`;
+  document.getElementById("minute-hand").style.transform = `rotate(${rotationMin}deg)`;
+  document.getElementById("second-hand").style.transform = `rotate(${rotationSec}deg)`;
+}
+
+setInterval(display, 1000);
